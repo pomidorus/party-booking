7b9692f3b423c4586a9ac4cc1c86977a4fa5ac18
davidrs <david.rs@hotmail.com>
2013-06-06 14:21:42 -0400

    Dave's test submit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyBookings::Application.config.secret_token = '3c8242886be7e19b21f3e3caa498d1feee1525dd3d4cf392bdbb0fa8cdcf47a8b8b7e1235469734f2dce5d6397381dc7e726225aa3cc738b8c4ba43f058b87e8'
